fb54d32c5f9b7618e0411ed192040863a5a507b8
Jhonathan Vitor <jhonathanvitor@gmail.com>
2022-09-19 23:49:42 -0400

    created orm prima

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,57 @@
 import express from "express"
+import { PrismaClient } from ".prisma/client";
 
 const app = express()
+const prisma = new PrismaClient({
+  log: [ 'query']
+})
 
-//ROTS = www.minhaapi.com/ads
+//HTTP methods / API RESTful / HTTP codes
 
-app.get('/ads', (request, response) => {
-  return response.json([
-    { id: 1, name: 'Anúncio 1' },
-    { id: 2, name: 'Anúncio 2' },
-    { id: 3, name: 'Anúncio 3' }
-  ])
+//ROTS = GET www.minhaapi.com/games
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+
+  return response.json(games);
+})
+
+//ROTS = POST www.minhaapi.com/games/ads
+app.post('/ads', (request, response) => {
+
+  return response.json([]);
+})
+
+//ROTS = GET www.minhaapi.com/ads
+app.get('/ads/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    }
+  })
+  
+  return response.json(ads)
 })
 
 //ROTS CORRETA = http://localhost:3333/ads
